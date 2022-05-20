3e5167d1a2ff1651e7843b70422160750a0971fb
Aditya23489e <99119876+Aditya23489e@users.noreply.github.com>
2022-05-21 00:31:42 +0530

    first commit

---
+++ static/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./components/hello"
+function App() {
+  return (
+    <div className="App">
+     <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ static/src/components/hello.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hello=()=>{
+    return (
+        <div>
+     <div>
+    <h1>Mobile Operating System</h1>
+    <li>Android</li>
+    <li>Blackberry</li>
+    <li>iphone</li>
+    <li>windows phone</li>
+    </div>
+    <div>
+           <h1>Mobile Manufacturers</h1>
+    <li>samsung</li>
+    <li>HTC</li>
+    <li>Micremax</li>
+    <li>Apple</li>
+    </div>
+    </div>
+    )
+
+}
+export default Hello;
